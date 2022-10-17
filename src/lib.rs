98d8970eca2ffc16aa12ce5ccfebe5c1f17a57a4
C. Titus Brown <titus@idyll.org>
2022-10-17 06:26:52 -0700

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,208 @@
+use pyo3::prelude::*;
+
+use std::fs::File;
+use std::io::{BufRead, BufReader, BufWriter, Write};
+use std::path::{Path, PathBuf};
+use std::sync::atomic::{AtomicUsize, Ordering};
+
+use log::{error, info};
+use rayon::prelude::*;
+use sourmash::signature::{Signature, SigsTrait};
+use sourmash::sketch::minhash::{max_hash_for_scaled, KmerMinHash};
+use sourmash::sketch::Sketch;
+
+fn check_compatible_downsample(
+    me: &KmerMinHash,
+    other: &KmerMinHash,
+) -> Result<(), sourmash::Error> {
+    /*
+    if self.num != other.num {
+        return Err(Error::MismatchNum {
+            n1: self.num,
+            n2: other.num,
+        }
+        .into());
+    }
+    */
+    use sourmash::Error;
+
+    if me.ksize() != other.ksize() {
+        return Err(Error::MismatchKSizes);
+    }
+    if me.hash_function() != other.hash_function() {
+        // TODO: fix this error
+        return Err(Error::MismatchDNAProt);
+    }
+    if me.max_hash() < other.max_hash() {
+        return Err(Error::MismatchScaled);
+    }
+    if me.seed() != other.seed() {
+        return Err(Error::MismatchSeed);
+    }
+    Ok(())
+}
+
+fn prepare_query(search_sig: &Signature, template: &Sketch) -> Option<KmerMinHash> {
+    let mut search_mh = None;
+    if let Some(Sketch::MinHash(mh)) = search_sig.select_sketch(template) {
+        search_mh = Some(mh.clone());
+    } else {
+        // try to find one that can be downsampled
+        if let Sketch::MinHash(template_mh) = template {
+            for sketch in search_sig.sketches() {
+                if let Sketch::MinHash(ref_mh) = sketch {
+                    if check_compatible_downsample(&ref_mh, template_mh).is_ok() {
+                        let max_hash = max_hash_for_scaled(template_mh.scaled());
+                        let mh = ref_mh.downsample_max_hash(max_hash).unwrap();
+                        return Some(mh);
+                    }
+                }
+            }
+        }
+    }
+    search_mh
+}
+
+fn search<P: AsRef<Path>>(
+    querylist: P,
+    siglist: P,
+    threshold: f64,
+    ksize: u8,
+    scaled: usize,
+    output: Option<P>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    info!("Loading queries");
+
+    let querylist_file = BufReader::new(File::open(querylist)?);
+
+    let max_hash = max_hash_for_scaled(scaled as u64);
+    let template_mh = KmerMinHash::builder()
+        .num(0u32)
+        .ksize(ksize as u32)
+        .max_hash(max_hash)
+        .build();
+    let template = Sketch::MinHash(template_mh);
+
+    let queries: Vec<(String, KmerMinHash)> = querylist_file
+        .lines()
+        .filter_map(|line| {
+            let line = line.unwrap();
+            if !line.is_empty() {
+                // skip empty lines
+                let mut path = PathBuf::new();
+                path.push(line);
+                Some(path)
+            } else {
+                None
+            }
+        })
+        .filter_map(|query| {
+            let query_sig = Signature::from_path(query).unwrap();
+
+            let mut query = None;
+            for sig in &query_sig {
+                if let Some(mh) = prepare_query(sig, &template) {
+                    query = Some((sig.name(), mh));
+                    break;
+                }
+            }
+            query
+        })
+        .collect();
+
+    if queries.is_empty() {
+        info!("No query signatures loaded, exiting.");
+        return Ok(());
+    }
+
+    info!("Loaded {} query signatures", queries.len());
+
+    info!("Loading siglist");
+    let siglist_file = BufReader::new(File::open(siglist)?);
+    let search_sigs: Vec<PathBuf> = siglist_file
+        .lines()
+        .filter_map(|line| {
+            let line = line.unwrap();
+            if !line.is_empty() {
+                let mut path = PathBuf::new();
+                path.push(line);
+                Some(path)
+            } else {
+                None
+            }
+        })
+        .collect();
+    info!("Loaded {} sig paths in siglist", search_sigs.len());
+
+    let processed_sigs = AtomicUsize::new(0);
+
+    let (send, recv) = std::sync::mpsc::sync_channel(rayon::current_num_threads());
+
+    // Spawn a thread that is dedicated to printing to a buffered output
+    let out: Box<dyn Write + Send> = match output {
+        Some(path) => Box::new(BufWriter::new(File::create(path).unwrap())),
+        None => Box::new(std::io::stdout()),
+    };
+    let thrd = std::thread::spawn(move || {
+        let mut writer = BufWriter::new(out);
+        writeln!(&mut writer, "query,Run,containment").unwrap();
+        for (query, m, containment) in recv.into_iter() {
+            writeln!(&mut writer, "'{}','{}',{}", query, m, containment).unwrap();
+        }
+    });
+
+    let send = search_sigs
+        .par_iter()
+        .filter_map(|filename| {
+            let i = processed_sigs.fetch_add(1, Ordering::SeqCst);
+            if i % 1000 == 0 {
+                info!("Processed {} search sigs", i);
+            }
+
+            let mut search_mh = None;
+            let search_sig = &Signature::from_path(&filename)
+                .unwrap_or_else(|_| panic!("Error processing {:?}", filename))[0];
+
+            if let Some(mh) = prepare_query(search_sig, &template) {
+                search_mh = Some(mh);
+            }
+            let search_mh = search_mh.unwrap();
+
+            let match_fn = filename.clone().into_os_string().into_string().unwrap();
+            let mut results = vec![];
+
+            for (name, query) in &queries {
+                let containment =
+                    query.count_common(&search_mh, false).unwrap() as f64 / query.size() as f64;
+                if containment > threshold {
+                    results.push((name.clone(), match_fn.clone(), containment))
+                }
+            }
+            if results.is_empty() {
+                None
+            } else {
+                Some(results)
+            }
+        })
+        .flatten()
+        .try_for_each_with(send, |s, m| s.send(m));
+
+    if let Err(e) = send {
+        error!("Unable to send internal data: {:?}", e);
+    }
+
+    if let Err(e) = thrd.join() {
+        error!("Unable to join internal thread: {:?}", e);
+    }
+
+    let i: usize = processed_sigs.fetch_max(0, Ordering::SeqCst);
+    info!("DONE. Processed {} search sigs", i);
+    Ok(())
+}
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn pymagsearch(_py: Python, m: &PyModule) -> PyResult<()> {
+//    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    Ok(())
+}
