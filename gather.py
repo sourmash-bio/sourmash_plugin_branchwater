456c9caee97b91da721e89e0cc0bddd3ed4c1ed5
C. Titus Brown <titus@idyll.org>
2022-10-19 06:55:01 -0700

    add gather.py

---
+++ gather.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+import sys
+import argparse
+import pymagsearch
+
+
+def main():
+    p = argparse.ArgumentParser()
+    p.add_argument('query')
+    p.add_argument('against_paths')
+    p.add_argument('-t', '--threshold', default=1000, type=int)
+    p.add_argument('-k', '--ksize', default=31, type=int)
+    p.add_argument('-s', '--scaled', default=1000, type=int)
+    p.add_argument('--output-prefetch', required=True)
+    p.add_argument('--output-gather', required=True)
+    args = p.parse_args()
+
+    pymagsearch.do_countergather(args.query,
+                                 args.against_paths,
+                                 args.threshold,
+                                 args.ksize,
+                                 args.scaled,
+                                 args.output_prefetch,
+                                 args.output_gather)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
