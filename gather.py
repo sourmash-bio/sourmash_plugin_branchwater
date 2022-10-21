1a7cea19a25f5d70f4ad8efe49c7502329e6a890
C. Titus Brown <titus@idyll.org>
2022-10-21 07:06:34 -0700

    change default threshold for gather to match sourmahs

--- gather.py
+++ gather.py
@@ -8,7 +8,7 @@ def main():
     p = argparse.ArgumentParser()
     p.add_argument('query')
     p.add_argument('against_paths')
-    p.add_argument('-t', '--threshold-bp', default=100000, type=int)
+    p.add_argument('-t', '--threshold-bp', default=50000, type=int)
     p.add_argument('-k', '--ksize', default=31, type=int)
     p.add_argument('-s', '--scaled', default=1000, type=int)
     p.add_argument('--output-prefetch', required=True)
