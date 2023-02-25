2ccdce4377ced4de1772dc816ee8432fe0a19e14
C. Titus Brown <titus@idyll.org>
2023-02-25 06:03:39 -0800

    add an actual test

--- python/tests/test_basic.py
+++ python/tests/test_basic.py
@@ -6,11 +6,35 @@ import sourmash_tst_utils as utils
 
 def get_test_data(filename):
     thisdir = os.path.dirname(__file__)
-    return os.path.join(thisdir, filename)
+    return os.path.join(thisdir, 'test-data', filename)
 
 
-def test_install(runtmp):
+def make_file_list(filename, paths):
+    with open(filename, 'wt') as fp:
+        fp.write("\n".join(paths))
+        fp.write("\n")
+
+
+def test_installed(runtmp):
     with pytest.raises(utils.SourmashCommandFailed):
         runtmp.sourmash('scripts', 'manysearch')
 
     assert 'usage:  manysearch' in runtmp.last_result.err
+
+
+def test_simple(runtmp):
+    query_list = runtmp.output('query.txt')
+    against_list = runtmp.output('against.txt')
+
+    sig2 = get_test_data('2.fa.sig.gz')
+    sig47 = get_test_data('47.fa.sig.gz')
+    sig63 = get_test_data('63.fa.sig.gz')
+
+    make_file_list(query_list, [sig2, sig47, sig63])
+    make_file_list(against_list, [sig2, sig47, sig63])
+
+    output = runtmp.output('out.csv')
+
+    runtmp.sourmash('scripts', 'manysearch', query_list, against_list,
+                    '-o', output)
+    assert os.path.exists(output)
