75bb659a6391d4ae878629e2238aebc6448faffa
Tessa Pierce Ward <bluegenes@users.noreply.github.com>
2023-09-20 17:51:33 -0700

    MRG: actually iterate through reads (#123)
    * fasta iteration from disk
    
    * rustfmt
    
    * comments
    
    * fix count
    
    * better fix count
    
    * clean up
    
    * remove unused import

--- src/manysketch.rs
+++ src/manysketch.rs
@@ -3,11 +3,9 @@ use anyhow::{anyhow, Result};
 use rayon::prelude::*;
 
 use crate::utils::{load_fasta_fromfile, sigwriter, Params, ZipMessage};
-use needletail::parse_fastx_reader;
+use needletail::parse_fastx_file;
 use sourmash::cmd::ComputeParameters;
 use sourmash::signature::Signature;
-use std::fs::File;
-use std::io::Read;
 use std::path::Path;
 use std::sync::atomic;
 use std::sync::atomic::AtomicUsize;
@@ -183,14 +181,14 @@ pub fn manysketch<P: AsRef<Path> + Sync>(
     let send_result = fileinfo
         .par_iter()
         .filter_map(|(name, filename, moltype)| {
-            let i = processed_fastas.fetch_add(1, atomic::Ordering::SeqCst);
+            // increment processed_fastas counter; make 1-based for % reporting
+            let i = processed_fastas.fetch_add(1, atomic::Ordering::SeqCst) + 1;
             // progress report at threshold
             if i != 0 && i % reporting_threshold == 0 {
                 let percent_processed = ((i as f64 / n_fastas as f64) * 100.0).round();
                 eprintln!("Starting file {}/{} ({}%)", i, n_fastas, percent_processed);
             }
 
-            let mut data: Vec<u8> = vec![];
             // build sig templates from params
             let (mut sigs, sig_params) = build_siginfo(&params_vec, moltype, name, filename);
             // if no sigs to build, skip
@@ -199,49 +197,36 @@ pub fn manysketch<P: AsRef<Path> + Sync>(
                 return None;
             }
 
-            // parse fasta file and add to signature
-            match File::open(filename) {
-                Ok(mut f) => {
-                    let _ = f.read_to_end(&mut data);
-
-                    match parse_fastx_reader(&data[..]) {
-                        Ok(mut parser) => {
-                            while let Some(record_result) = parser.next() {
-                                match record_result {
-                                    Ok(record) => {
-                                        for sig in &mut sigs {
-                                            if moltype == "protein" {
-                                                sig.add_protein(&record.seq()).unwrap();
-                                            } else {
-                                                sig.add_sequence(&record.seq(), true).unwrap();
-                                                // if not force, panics with 'N' in dna sequence
-                                            }
-                                        }
-                                    }
-                                    Err(error) => {
-                                        eprintln!("Error while processing record: {:?}", error);
-                                    }
-                                }
-                            }
-                            Some((sigs, sig_params, filename))
-                        }
-                        Err(err) => {
-                            eprintln!(
-                                "Error creating parser for file {}: {:?}",
-                                filename.display(),
-                                err
-                            );
-                            let _ = failed_paths.fetch_add(1, atomic::Ordering::SeqCst);
-                            None
-                        }
-                    }
-                }
+            // Open fasta file reader
+            let mut reader = match parse_fastx_file(&filename) {
+                Ok(r) => r,
                 Err(err) => {
                     eprintln!("Error opening file {}: {:?}", filename.display(), err);
                     let _ = failed_paths.fetch_add(1, atomic::Ordering::SeqCst);
-                    None
+                    return None;
+                }
+            };
+            // parse fasta and add to signature
+            while let Some(record_result) = reader.next() {
+                match record_result {
+                    Ok(record) => {
+                        // do we need to normalize to make sure all the bases are consistently capitalized?
+                        // let norm_seq = record.normalize(false);
+                        for sig in &mut sigs {
+                            if moltype == "protein" {
+                                sig.add_protein(&record.seq()).unwrap();
+                            } else {
+                                sig.add_sequence(&record.seq(), true).unwrap();
+                                // if not force, panics with 'N' in dna sequence
+                            }
+                        }
+                    }
+                    Err(err) => {
+                        eprintln!("Error while processing record: {:?}", err);
+                    }
                 }
             }
+            Some((sigs, sig_params, filename))
         })
         .try_for_each_with(
             send.clone(),
