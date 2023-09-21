sourmash-bio/sourmash_plugin_branchwater

commit 75bb659a6391d4ae878629e2238aebc6448faffa

MRG: actually iterate through reads (#123)
* fasta iteration from disk

* rustfmt

* comments

* fix count

* better fix count

* clean up

* remove unused import

src/manysketch.rs
@@ -3,11 +3,9 @@ use anyhow::{anyhow, Result};
 use rayon::prelude::*;
 
 use crate::utils::{load_fasta_fromfile, sigwriter, Params, ZipMessage};
-use needletail::parse_fastx_reader;
+use needletail::parse_fastx_file;
 use sourmash::cmd::ComputeParameters;
 use sourmash::signature::Signature;
-use std::fs::File;
-use std::io::Read;
 use std::path::Path;
 use std::sync::atomic;
 use std::sync::atomic::AtomicUsize;
@@ -183,14 +181,14 @@ pub fn manysketch<P: AsRef<Path> + Sync>(
     let send_result = fileinfo
         .par_iter()
         .filter_map(|(name, filename, moltype)| {
-            let i = processed_fastas.fetch_add(1, atomic::Ordering::SeqCst);
+            // increment processed_fastas counter; make 1-based for % reporting
+            let i = processed_fastas.fetch_add(1, atomic::Ordering::SeqCst) + 1;
             // progress report at threshold
             if i != 0 && i % reporting_threshold == 0 {
                 let percent_processed = ((i as f64 / n_fastas as f64) * 100.0).round();
                 eprintln!("Starting file {}/{} ({}%)", i, n_fastas, percent_processed);
             }
 
-            let mut data: Vec<u8> = vec![];
             // build sig templates from params
             let (mut sigs, sig_params) = build_siginfo(&params_vec, moltype, name, filename);
             // if no sigs to build, skip
@@ -199,49 +197,36 @@ pub fn manysketch<P: AsRef<Path> + Sync>(
                 return None;
             }
 
-            // parse fasta file and add to signature
-            match File::open(filename) {
-                Ok(mut f) => {
-                    let _ = f.read_to_end(&mut data);
-
-                    match parse_fastx_reader(&data[..]) {
-                        Ok(mut parser) => {
-                            while let Some(record_result) = parser.next() {
-                                match record_result {
-                                    Ok(record) => {
-                                        for sig in &mut sigs {
-                                            if moltype == "protein" {
-                                                sig.add_protein(&record.seq()).unwrap();
-                                            } else {
-                                                sig.add_sequence(&record.seq(), true).unwrap();
-                                                // if not force, panics with 'N' in dna sequence
-                                            }
-                                        }
-                                    }
-                                    Err(error) => {
-                                        eprintln!("Error while processing record: {:?}", error);
-                                    }
-                                }
-                            }
-                            Some((sigs, sig_params, filename))
-                        }
-                        Err(err) => {
-                            eprintln!(
-                                "Error creating parser for file {}: {:?}",
-                                filename.display(),
-                                err
-                            );
-                            let _ = failed_paths.fetch_add(1, atomic::Ordering::SeqCst);
-                            None
-                        }
-                    }
-                }
+            // Open fasta file reader
+            let mut reader = match parse_fastx_file(&filename) {
+                Ok(r) => r,
                 Err(err) => {
                     eprintln!("Error opening file {}: {:?}", filename.display(), err);
                     let _ = failed_paths.fetch_add(1, atomic::Ordering::SeqCst);
-                    None
+                    return None;
+                }
+            };
+            // parse fasta and add to signature
+            while let Some(record_result) = reader.next() {
+                match record_result {
+                    Ok(record) => {
+                        // do we need to normalize to make sure all the bases are consistently capitalized?
+                        // let norm_seq = record.normalize(false);
+                        for sig in &mut sigs {
+                            if moltype == "protein" {
+                                sig.add_protein(&record.seq()).unwrap();
+                            } else {
+                                sig.add_sequence(&record.seq(), true).unwrap();
+                                // if not force, panics with 'N' in dna sequence
+                            }
+                        }
+                    }
+                    Err(err) => {
+                        eprintln!("Error while processing record: {:?}", err);
+                    }
                 }
             }
+            Some((sigs, sig_params, filename))
         })
         .try_for_each_with(
             send.clone(),

commit b67fa0d9a1f15c371361922a9a34ac1d10a5d477

MRG: fix reporting bug introduced in #123 (#126)
* fix reporting

* rustfmt

* add only incompatible test and reporting

* 1-based reporting

src/manysketch.rs
@@ -182,11 +182,16 @@ pub fn manysketch<P: AsRef<Path> + Sync>(
         .par_iter()
         .filter_map(|(name, filename, moltype)| {
             // increment processed_fastas counter; make 1-based for % reporting
-            let i = processed_fastas.fetch_add(1, atomic::Ordering::SeqCst) + 1;
+            let i = processed_fastas.fetch_add(1, atomic::Ordering::SeqCst);
             // progress report at threshold
-            if i != 0 && i % reporting_threshold == 0 {
-                let percent_processed = ((i as f64 / n_fastas as f64) * 100.0).round();
-                eprintln!("Starting file {}/{} ({}%)", i, n_fastas, percent_processed);
+            if (i + 1) % reporting_threshold == 0 {
+                let percent_processed = (((i + 1) as f64 / n_fastas as f64) * 100.0).round();
+                eprintln!(
+                    "Starting file {}/{} ({}%)",
+                    (i + 1),
+                    n_fastas,
+                    percent_processed
+                );
             }
 
             // build sig templates from params
@@ -264,7 +269,7 @@ pub fn manysketch<P: AsRef<Path> + Sync>(
     }
 
     // done!
-    let i: usize = processed_fastas.fetch_max(0, atomic::Ordering::SeqCst);
+    let i: usize = processed_fastas.load(atomic::Ordering::SeqCst);
     eprintln!("DONE. Processed {} fasta files", i);
 
     let failed_paths = failed_paths.load(atomic::Ordering::SeqCst);
@@ -280,6 +285,9 @@ pub fn manysketch<P: AsRef<Path> + Sync>(
     }
 
     let skipped_paths = skipped_paths.load(atomic::Ordering::SeqCst);
+    if skipped_paths == i {
+        bail!("No fasta files compatible with provided sketch parameters: no signatures created.");
+    }
     if skipped_paths > 0 {
         eprintln!(
             "WARNING: {} fasta files skipped - no compatible signatures.",

src/python/tests/test_sketch.py
@@ -147,6 +147,32 @@ def test_manysketch_mult_moltype(runtmp):
             assert sig.md5sum() in ["4efeebd26644278e36b9553e018a851a","f85747ac4f473c4a71c1740d009f512b"]
 
 
+def test_manysketch_only_incompatible_fastas(runtmp, capfd):
+    # provide dna, protein fastas, but only sketch protein (skip protein fastas!)
+    fa_csv = runtmp.output('db-fa.csv')
+
+    fa1 = get_test_data('short.fa')
+    fa2 = get_test_data('short2.fa')
+    fa3 = get_test_data('short3.fa')
+
+    make_file_csv(fa_csv, [fa1, fa2, fa3])
+
+    output = runtmp.output('db.zip')
+
+    with pytest.raises(utils.SourmashCommandFailed):
+        runtmp.sourmash('scripts', 'manysketch', fa_csv, '-o', output,
+                        '--param-str', "protein,k=10,scaled=1")
+
+    assert os.path.exists(output) # output will still exist - is this desired?
+    assert not runtmp.last_result.out # stdout should be empty
+
+    captured = capfd.readouterr()
+    print(captured.err)
+
+    assert 'DONE. Processed 3 fasta files' in captured.err
+    assert 'Error: No fasta files compatible with provided sketch parameters: no signatures created.' in captured.err
+
+
 def test_manysketch_skip_incompatible_fastas(runtmp, capfd):
     # provide dna, protein fastas, but only sketch protein (skip protein fastas!)
     fa_csv = runtmp.output('db-fa.csv')
@@ -179,6 +205,10 @@ def test_manysketch_skip_incompatible_fastas(runtmp, capfd):
         assert sig.minhash.ksize == 10
         assert sig.minhash.scaled == 1
         assert sig.md5sum() == "eb4467d11e0ecd2dbde4193bfc255310"
+    assert 'Starting file 2/4 (50%)' in captured.err
+    assert 'Starting file 3/4 (75%)' in captured.err
+    assert 'Starting file 4/4 (100%)' in captured.err
+    assert 'DONE. Processed 4 fasta files' in captured.err
     assert 'WARNING: 3 fasta files skipped - no compatible signatures.' in captured.err
 
 