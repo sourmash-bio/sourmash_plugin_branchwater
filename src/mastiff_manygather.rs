243d1067178c37763029263cd16084ae9e569199
N. Tessa Pierce-Ward <ntpierce@gmail.com>
2024-01-23 15:16:10 -0800

    rustfmt

--- src/mastiff_manygather.rs
+++ src/mastiff_manygather.rs
@@ -4,8 +4,8 @@ use rayon::prelude::*;
 
 use sourmash::ffi::signature;
 use sourmash::signature::Signature;
-use sourmash::sketch::Sketch;
 use sourmash::sketch::minhash::KmerMinHash;
+use sourmash::sketch::Sketch;
 use std::path::Path;
 
 // use sourmash::collection::Collection;
@@ -85,88 +85,92 @@ pub fn mastiff_manygather<P: AsRef<Path>>(
     let skipped_paths = AtomicUsize::new(0);
     let failed_paths = AtomicUsize::new(0);
 
-
     let send = query_paths
-    .par_iter()
-    .filter_map(|filename| {
-        // ... existing setup code ...
-    let threshold = threshold_bp / selection.scaled()? as usize;
+        .par_iter()
+        .filter_map(|filename| {
+            // ... existing setup code ...
+            let threshold = threshold_bp / selection.scaled()? as usize;
 
-        match Signature::from_path(filename) {
-            Ok(mut signatures) if !signatures.is_empty() => {
-                match signatures.swap_remove(0).select(&selection) {
-                    Ok(query_sig) => {
-                        let mut results = vec![];
-                        let mut found_compatible_sketch = false;
-                        for sketch in query_sig.iter() {
-                            if let Sketch::MinHash(query) = sketch {
-                                found_compatible_sketch = true;
-                                // eprintln!("query-size: {}", sketch.size());
-                                // Gather!
-                                let (counter, query_colors, hash_to_color) = db.prepare_gather_counters(&query);
+            match Signature::from_path(filename) {
+                Ok(mut signatures) if !signatures.is_empty() => {
+                    match signatures.swap_remove(0).select(&selection) {
+                        Ok(query_sig) => {
+                            let mut results = vec![];
+                            let mut found_compatible_sketch = false;
+                            for sketch in query_sig.iter() {
+                                if let Sketch::MinHash(query) = sketch {
+                                    found_compatible_sketch = true;
+                                    // eprintln!("query-size: {}", sketch.size());
+                                    // Gather!
+                                    let (counter, query_colors, hash_to_color) =
+                                        db.prepare_gather_counters(&query);
 
-                                let matches = db.gather(
-                                    counter,
-                                    query_colors,
-                                    hash_to_color,
-                                    threshold,
-                                    &query,
-                                    Some(selection.clone()),
-                                );
-                                // extract results
-                                if let Ok(matches) = matches {
-                                    for match_ in &matches {
-                                        results.push((
-                                            query_sig.name().clone(),
-                                            query.md5sum().clone(),
-                                            match_.name().clone(),
-                                            match_.md5().clone(),
-                                            match_.f_match(), // f_match_query
-                                            match_.intersect_bp(),
-                                        )); // intersect_bp
+                                    let matches = db.gather(
+                                        counter,
+                                        query_colors,
+                                        hash_to_color,
+                                        threshold,
+                                        &query,
+                                        Some(selection.clone()),
+                                    );
+                                    // extract results
+                                    if let Ok(matches) = matches {
+                                        for match_ in &matches {
+                                            results.push((
+                                                query_sig.name().clone(),
+                                                query.md5sum().clone(),
+                                                match_.name().clone(),
+                                                match_.md5().clone(),
+                                                match_.f_match(), // f_match_query
+                                                match_.intersect_bp(),
+                                            )); // intersect_bp
+                                        }
+                                    } else {
+                                        eprintln!("Error gathering matches: {:?}", matches.err());
                                     }
-                                } else {
-                                    eprintln!("Error gathering matches: {:?}", matches.err());
                                 }
                             }
-                        }
-                        if !found_compatible_sketch {
-                            if !queryfile_name.ends_with(".zip") {
-                                eprintln!(
-                                    "WARNING: no compatible sketches in path '{}'",
-                                    filename.display()
-                                );
+                            if !found_compatible_sketch {
+                                if !queryfile_name.ends_with(".zip") {
+                                    eprintln!(
+                                        "WARNING: no compatible sketches in path '{}'",
+                                        filename.display()
+                                    );
+                                }
+                                let _ = skipped_paths.fetch_add(1, atomic::Ordering::SeqCst);
                             }
-                            let _ = skipped_paths.fetch_add(1, atomic::Ordering::SeqCst);
-                        }
 
-                        if results.is_empty() {
-                            None
-                        } else {
-                            Some(results)
+                            if results.is_empty() {
+                                None
+                            } else {
+                                Some(results)
+                            }
                         }
-                    }
-                    Err(err) => {
-                        eprintln!("Error selecting sketches: {}", err);
-                        let _ = failed_paths.fetch_add(1, atomic::Ordering::SeqCst);
-                        None
+                        Err(err) => {
+                            eprintln!("Error selecting sketches: {}", err);
+                            let _ = failed_paths.fetch_add(1, atomic::Ordering::SeqCst);
+                            None
+                        }
                     }
                 }
+                Ok(_) => {
+                    eprintln!("No signatures found in '{}'", filename.display());
+                    let _ = failed_paths.fetch_add(1, atomic::Ordering::SeqCst);
+                    None
+                }
+                Err(err) => {
+                    eprintln!(
+                        "WARNING: could not load sketches from path '{}': {}",
+                        filename.display(),
+                        err
+                    );
+                    let _ = failed_paths.fetch_add(1, atomic::Ordering::SeqCst);
+                    None
+                }
             }
-            Ok(_) => {
-                eprintln!("No signatures found in '{}'", filename.display());
-                let _ = failed_paths.fetch_add(1, atomic::Ordering::SeqCst);
-                None
-            }
-            Err(err) => {
-                eprintln!("WARNING: could not load sketches from path '{}': {}", filename.display(), err);
-                let _ = failed_paths.fetch_add(1, atomic::Ordering::SeqCst);
-                None
-            }
-        }
-    })
-    .flatten()
-    .try_for_each_with(send, |s, m| s.send(m));
+        })
+        .flatten()
+        .try_for_each_with(send, |s, m| s.send(m));
 
     // do some cleanup and error handling -
     if let Err(e) = send {
