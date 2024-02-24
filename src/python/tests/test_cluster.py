6929f5002968fc430b8a048ef7beafc3963aaf7f
N. Tessa Pierce-Ward <ntpierce@gmail.com>
2024-02-23 16:10:50 -0800

    add tests for max ani, max contain

--- src/python/tests/test_cluster.py
+++ src/python/tests/test_cluster.py
@@ -38,6 +38,73 @@ def test_cluster_containment(runtmp):
     assert rows[0]['count'] == '1'
 
 
+def test_cluster_max_containment_1(runtmp):
+    pairwise_csv = get_test_data('cluster.pairwise.csv')
+    output = runtmp.output('clusters.csv')
+    sizes = runtmp.output('sizes.csv')
+    threshold = '0.7'
+
+    runtmp.sourmash('scripts', 'cluster', pairwise_csv, '-o', output,
+                    '--similarity-column', "max_containment", "--cluster-sizes",
+                    sizes, '--threshold', threshold)
+
+    assert os.path.exists(output)
+
+    # check cluster output
+    with open(output, mode='r', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        rows = [row for row in reader]
+        assert reader.fieldnames == ['cluster','nodes']
+    assert len(rows) == 1, f"Expected 1 data row but found {len(rows)}"
+    assert rows[0]['cluster'] == 'Component_1'
+    expected = set("n2;n3;n7;n1;n6;n5;n4".split(';'))
+    assert set(rows[0]['nodes'].split(';')) == expected
+
+    # check cluster size histogram
+    with open(sizes, mode='r', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        rows = [row for row in reader]
+        assert reader.fieldnames == ['cluster_size','count']
+    assert len(rows) == 1, f"Expected 1 data row but found {len(rows)}"
+    assert rows[0]['cluster_size'] == '7'
+    assert rows[0]['count'] == '1'
+
+
+def test_cluster_max_containment_2(runtmp):
+    pairwise_csv = get_test_data('cluster.pairwise.csv')
+    output = runtmp.output('clusters.csv')
+    sizes = runtmp.output('sizes.csv')
+    threshold = '0.9'
+
+    runtmp.sourmash('scripts', 'cluster', pairwise_csv, '-o', output,
+                    '--similarity-column', "max_containment", "--cluster-sizes",
+                    sizes, '--threshold', threshold)
+
+    assert os.path.exists(output)
+
+    # check cluster output
+    with open(output, mode='r', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        rows = [row for row in reader]
+        assert reader.fieldnames == ['cluster','nodes']
+    assert len(rows) == 2, f"Expected 2 data rows but found {len(rows)}"
+    assert rows[0]['cluster'] == 'Component_1'
+    expected = set("n1;n2;n3;n4;n5".split(';'))
+    assert set(rows[0]['nodes'].split(';')) == expected
+    expected = set("n6;n7".split(';'))
+    assert set(rows[1]['nodes'].split(';')) == expected
+
+    # check cluster size histogram
+    with open(sizes, mode='r', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        rows = [row for row in reader]
+        assert reader.fieldnames == ['cluster_size','count']
+    assert len(rows) == 2, f"Expected 2 data rows but found {len(rows)}"
+    rows_as_tuples = {tuple(row.values()) for row in rows}
+    expected = {('5', '1'), ('2', '1')}
+    assert rows_as_tuples == expected
+
+
 def test_cluster_jaccard(runtmp): 
     pairwise_csv = get_test_data('cluster.pairwise.csv')
     output = runtmp.output('clusters.csv')
@@ -91,6 +158,43 @@ def test_cluster_ani(runtmp):
     assert rows[0]['cluster'] == 'Component_1'
     expected = set("n1;n2;n3;n4;n5".split(';'))
     assert set(rows[0]['nodes'].split(';')) == expected
+    expected = set("n6;n7".split(';'))
+    assert set(rows[1]['nodes'].split(';')) == expected
+
+    # check cluster size histogram
+    with open(sizes, mode='r', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        rows = [row for row in reader]
+        assert reader.fieldnames == ['cluster_size','count']
+    assert len(rows) == 2, f"Expected 2 data rows but found {len(rows)}"
+    rows_as_tuples = {tuple(row.values()) for row in rows}
+    expected = {('5', '1'), ('2', '1')}
+    assert rows_as_tuples == expected
+
+
+def test_cluster_max_ani(runtmp):
+    pairwise_csv = get_test_data('cluster.pairwise.csv')
+    output = runtmp.output('clusters.csv')
+    sizes = runtmp.output('sizes.csv')
+    threshold = '0.9'
+
+    runtmp.sourmash('scripts', 'cluster', pairwise_csv, '-o', output,
+                    '--similarity-column', "max_ani", "--cluster-sizes",
+                    sizes, '--threshold', threshold)
+
+    assert os.path.exists(output)
+
+    # check cluster output
+    with open(output, mode='r', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        rows = [row for row in reader]
+        assert reader.fieldnames == ['cluster','nodes']
+    assert len(rows) == 2, f"Expected 2 data rows but found {len(rows)}"
+    assert rows[0]['cluster'] == 'Component_1'
+    expected = set("n1;n2;n3;n4;n5".split(';'))
+    assert set(rows[0]['nodes'].split(';')) == expected
+    expected = set("n6;n7".split(';'))
+    assert set(rows[1]['nodes'].split(';')) == expected
 
     # check cluster size histogram
     with open(sizes, mode='r', newline='') as csvfile:
