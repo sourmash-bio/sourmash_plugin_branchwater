94ebad45035ecd8be2ba780f56f056a644a6d5c7
N. Tessa Pierce-Ward <ntpierce@gmail.com>
2024-02-27 18:07:15 -0800

    add ctrl-c for manysearch

--- src/manysearch.rs
+++ src/manysearch.rs
@@ -6,9 +6,13 @@
 use anyhow::Result;
 use rayon::prelude::*;
 use std::sync::atomic;
-use std::sync::atomic::AtomicUsize;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::Arc;
+use std::sync::Mutex;
 
-use crate::utils::{csvwriter_thread, load_collection, load_sketches, ReportType, SearchResult};
+use crate::utils::{
+    csvwriter_thread, load_collection, load_sketches, ReportType, SearchResult, ThreadManager,
+};
 use sourmash::selection::Selection;
 use sourmash::signature::SigsTrait;
 
@@ -44,6 +48,12 @@ pub fn manysearch(
     // & spawn a thread that is dedicated to printing to a buffered output
     let thrd = csvwriter_thread(recv, output);
 
+    // set up manager to allow for ctrl-c handling
+    let manager = ThreadManager::new(send, thrd);
+
+    // Wrap ThreadManager in Arc<Mutex> for safe sharing across threads
+    let manager_shared = Arc::new(Mutex::new(manager));
+
     //
     // Main loop: iterate (in parallel) over all search signature paths,
     // loading them individually and searching them. Stuff results into
@@ -54,75 +64,67 @@ pub fn manysearch(
     let skipped_paths = AtomicUsize::new(0);
     let failed_paths = AtomicUsize::new(0);
 
-    let send = against_collection
-        .par_iter()
-        .filter_map(|(_idx, record)| {
-            let i = processed_sigs.fetch_add(1, atomic::Ordering::SeqCst);
-            if i % 1000 == 0 {
-                eprintln!("Processed {} search sigs", i);
-            }
+    against_collection.par_iter().for_each(|(_idx, record)| {
+        let manager_clone = manager_shared.clone();
+        let i = processed_sigs.fetch_add(1, Ordering::SeqCst);
+        if i % 1000 == 0 {
+            eprintln!("Processed {} search sigs", i);
+        }
 
-            let mut results = vec![];
-
-            // against downsampling happens here
-            match against_collection.sig_from_record(record) {
-                Ok(against_sig) => {
-                    if let Some(against_mh) = against_sig.minhash() {
-                        for query in query_sketchlist.iter() {
-                            let overlap =
-                                query.minhash.count_common(against_mh, false).unwrap() as f64;
-                            let query_size = query.minhash.size() as f64;
-                            let target_size = against_mh.size() as f64;
-                            let containment_query_in_target = overlap / query_size;
-                            let containment_in_target = overlap / target_size;
-                            let max_containment =
-                                containment_query_in_target.max(containment_in_target);
-                            let jaccard = overlap / (target_size + query_size - overlap);
-
-                            if containment_query_in_target > threshold {
-                                results.push(SearchResult {
-                                    query_name: query.name.clone(),
-                                    query_md5: query.md5sum.clone(),
-                                    match_name: against_sig.name(),
-                                    containment: containment_query_in_target,
-                                    intersect_hashes: overlap as usize,
-                                    match_md5: Some(against_sig.md5sum()),
-                                    jaccard: Some(jaccard),
-                                    max_containment: Some(max_containment),
-                                });
-                            }
+        // against downsampling happens here
+        match against_collection.sig_from_record(record) {
+            Ok(against_sig) => {
+                if let Some(against_mh) = against_sig.minhash() {
+                    for query in query_sketchlist.iter() {
+                        if manager_clone
+                            .lock()
+                            .unwrap()
+                            .interrupted
+                            .load(Ordering::SeqCst)
+                        {
+                            // If interrupted, simply exit the closure
+                            return;
                         }
-                    } else {
-                        eprintln!(
-                            "WARNING: no compatible sketches in path '{}'",
-                            record.internal_location()
-                        );
-                        let _ = skipped_paths.fetch_add(1, atomic::Ordering::SeqCst);
+                        let overlap = query.minhash.count_common(against_mh, false).unwrap() as f64;
+                        let query_size = query.minhash.size() as f64;
+                        let target_size = against_mh.size() as f64;
+                        let containment_query_in_target = overlap / query_size;
+                        let containment_target_in_query = overlap / target_size;
+                        let jaccard = overlap / (target_size + query_size - overlap);
+                        let max_containment =
+                            containment_query_in_target.max(containment_target_in_query);
+
+                        if containment_query_in_target > threshold {
+                            let search_result = SearchResult {
+                                query_name: query.name.clone(),
+                                query_md5: query.md5sum.clone(),
+                                match_name: against_sig.name(),
+                                containment: containment_query_in_target,
+                                intersect_hashes: overlap as usize,
+                                match_md5: Some(against_sig.md5sum()),
+                                jaccard: Some(jaccard),
+                                max_containment: Some(max_containment),
+                            };
+                            manager_clone.lock().unwrap().send(search_result);
+                        }
                     }
-                }
-                Err(err) => {
-                    eprintln!("Sketch loading error: {}", err);
+                } else {
                     eprintln!(
                         "WARNING: no compatible sketches in path '{}'",
                         record.internal_location()
                     );
-                    let _ = skipped_paths.fetch_add(1, atomic::Ordering::SeqCst);
+                    skipped_paths.fetch_add(1, Ordering::SeqCst);
                 }
             }
-
-            Some(results)
-        })
-        .flatten()
-        .try_for_each_with(send, |s, m| s.send(m));
+            Err(err) => {
+                eprintln!("Sketch loading error: {}", err);
+                skipped_paths.fetch_add(1, Ordering::SeqCst);
+            }
+        }
+    });
 
     // do some cleanup and error handling -
-    if let Err(e) = send {
-        eprintln!("Unable to send internal data: {:?}", e);
-    }
-
-    if let Err(e) = thrd.join() {
-        eprintln!("Unable to join internal thread: {:?}", e);
-    }
+    manager_shared.lock().unwrap().perform_cleanup();
 
     // done!
     let i: usize = processed_sigs.fetch_max(0, atomic::Ordering::SeqCst);
