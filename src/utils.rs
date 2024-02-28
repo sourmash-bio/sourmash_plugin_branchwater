f246ac2bf890b66ff99c287ad9d1ddb8997890b5
N. Tessa Pierce-Ward <ntpierce@gmail.com>
2024-02-28 11:13:00 -0800

    ctrlc manysketch

--- src/utils.rs
+++ src/utils.rs
@@ -25,14 +25,14 @@ use sourmash::signature::{Signature, SigsTrait};
 use sourmash::sketch::minhash::KmerMinHash;
 use sourmash::storage::{FSStorage, InnerStorage, SigStore};
 
-pub struct ThreadManager<T: Serialize + Send + 'static> {
+pub struct ThreadManager<T: Send + 'static> {
     pub sender: Option<SyncSender<T>>,
-    pub writer_thread: Option<std::thread::JoinHandle<()>>,
+    pub writer_thread: Option<std::thread::JoinHandle<Result<()>>>,
     pub interrupted: Arc<AtomicBool>,
 }
 
-impl<T: Serialize + Send + 'static> ThreadManager<T> {
-    pub fn new(sender: SyncSender<T>, writer_thread: std::thread::JoinHandle<()>) -> Self {
+impl<T: Send + 'static> ThreadManager<T> {
+    pub fn new(sender: SyncSender<T>, writer_thread: std::thread::JoinHandle<Result<()>>) -> Self {
         let interrupted = Arc::new(AtomicBool::new(false));
         let interrupted_clone = interrupted.clone();
 
@@ -939,13 +939,15 @@ impl SearchResultTrait for MultiSearchResult {
     }
 }
 
-pub fn open_stdout_or_file(output: Option<String>) -> Box<dyn Write + Send + 'static> {
+pub fn open_stdout_or_file(output: Option<String>) -> Result<Box<dyn Write + Send>> {
     // if output is a file, use open_output_file
-    if let Some(path) = output {
-        let outpath: PathBuf = path.into();
-        Box::new(open_output_file(&outpath))
-    } else {
-        Box::new(std::io::stdout())
+    match output {
+        Some(path) => {
+            let path_buf = PathBuf::from(path);
+            let file_writer = open_output_file(&path_buf);
+            Ok(Box::new(file_writer) as Box<dyn Write + Send>)
+        }
+        None => Ok(Box::new(std::io::stdout()) as Box<dyn Write + Send>),
     }
 }
 
@@ -1026,14 +1028,15 @@ pub fn sigwriter(
                 ZipMessage::WriteManifest => {
                     println!("Writing manifest");
                     // Start the CSV file inside the zip
-                    zip.start_file("SOURMASH-MANIFEST.csv", options).unwrap();
+                    zip.start_file("SOURMASH-MANIFEST.csv", options)
+                        .context("Failed to start CSV file in ZIP")?;
                     let manifest: Manifest = manifest_rows.clone().into();
-                    manifest.to_writer(&mut zip)?;
+                    manifest
+                        .to_writer(&mut zip)
+                        .context("Failed to write manifest to ZIP")?;
 
                     // Properly finish writing to the ZIP file
-                    if let Err(e) = zip.finish() {
-                        eprintln!("Error finalizing ZIP file: {:?}", e);
-                    }
+                    zip.finish().context("Error finalizing ZIP file")?;
                 }
             }
         }
@@ -1041,22 +1044,24 @@ pub fn sigwriter(
     })
 }
 
-pub fn csvwriter_thread<T: Serialize + Send + SearchResultTrait + 'static>(
+pub fn csvwriter_thread<T: Serialize + Send + 'static>(
     recv: std::sync::mpsc::Receiver<T>,
     output: Option<String>,
-) -> std::thread::JoinHandle<()> {
-    // create output file
-    let out = open_stdout_or_file(output);
-    // spawn a thread that is dedicated to printing to a buffered output
-    std::thread::spawn(move || {
+) -> std::thread::JoinHandle<Result<()>> {
+    std::thread::spawn(move || -> Result<()> {
+        let out = open_stdout_or_file(output)?;
         let mut writer = Writer::from_writer(out);
 
         for res in recv.iter() {
-            if let Err(e) = writer.serialize(&res) {
-                eprintln!("Error writing item: {:?}", e);
-            }
+            writer
+                .serialize(&res)
+                .map_err(|e| anyhow!("Error writing item: {:?}", e))?;
         }
-        writer.flush().expect("Failed to flush writer.");
+        writer
+            .flush()
+            .map_err(|e| anyhow!("Failed to flush writer: {:?}", e))?;
+
+        Ok(())
     })
 }
 
