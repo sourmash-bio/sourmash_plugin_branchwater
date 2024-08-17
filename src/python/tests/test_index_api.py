87b4563aace15a9a3a472b5583e77cdac9fb2f0f
C. Titus Brown <titus@idyll.org>
2024-08-17 11:24:42 -0700

    y'know ...tests

---
+++ src/python/tests/test_index_api.py
@@ -0,0 +1,18 @@
+import sourmash_plugin_branchwater as branch
+from . import sourmash_tst_utils as utils
+from .sourmash_tst_utils import get_test_data
+
+
+def test_basic():
+    sigfile = get_test_data('SRR606249.sig.gz')
+    res = branch.api.api_load_collection(sigfile, 31, 100_000, 'DNA')
+    assert res.val == 1001
+
+
+def test_fail():
+    sigfile = get_test_data('XXX_SRR606249.sig.gz')
+    try:
+        res = branch.api.api_load_collection(sigfile, 31, 100_000, 'DNA')
+    except:
+        pass
+    # @CTB should do something better here ;)
