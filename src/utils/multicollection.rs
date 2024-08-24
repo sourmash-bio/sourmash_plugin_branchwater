e3e95fc5160735137b69104f6ff181e16dacf981
C. Titus Brown <titus@idyll.org>
2024-08-24 12:44:22 -0700

    impl Deref for MultiCollection

--- src/utils/multicollection.rs
+++ src/utils/multicollection.rs
@@ -9,6 +9,7 @@ use log::debug;
 use std::collections::HashSet;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::ops::Deref;
 use std::sync::atomic;
 use std::sync::atomic::AtomicUsize;
 
@@ -275,6 +276,14 @@ impl MultiCollection {
             .collect();
 
         Ok(sketchinfo)
+    }
+}
+
+impl Deref for MultiCollection {
+    type Target = Vec<Collection>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.collections
     }
 }
 
