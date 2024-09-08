561ed3c9560979f25a831b93a1b7df24091be7a5
C. Titus Brown <titus@idyll.org>
2024-09-08 17:42:20 -0400

    kinda working

--- src/index.rs
+++ src/index.rs
@@ -1,12 +1,9 @@
-use anyhow::Context;
-use camino::Utf8PathBuf as PathBuf;
 use sourmash::index::revindex::RevIndex;
 use sourmash::index::revindex::RevIndexOps;
 use sourmash::prelude::*;
-use std::fs::File;
-use std::io::{BufRead, BufReader};
 use std::path::Path;
 
+use sourmash::Error;
 use sourmash::collection::{Collection, CollectionSet};
 use crate::utils::{ load_collection, ReportType };
 use crate::utils::multicollection::MultiCollection;
@@ -24,25 +21,38 @@ pub fn index<P: AsRef<Path>>(
     let multi: MultiCollection = load_collection(&siglist, selection,
                                                  ReportType::General,
                                                  allow_failed_sigpaths).unwrap();
-    if multi.len() == 1 || use_internal_storage {
-        let mut collection: CollectionSet;
-        if multi.len() == 1 {
-            let coll: Collection = Collection::try_from(multi).unwrap();
-            collection = coll.select(selection)?.try_into().unwrap();
-        } else { // use_internal_storage
-            // @CTB warn: loading all the things
-            let coll = multi.load_all_sigs(selection).unwrap();
-            // @CTB multiple selects...
-             collection = coll.select(selection)?.try_into()?;
-        }
-        eprintln!("Indexing {} sketches.", collection.len());
-        let mut index = RevIndex::create(output.as_ref(), collection, colors)?;
+    eprintln!("loaded - {}", multi.len());
 
-        if use_internal_storage {
-            index.internalize_storage()?;
+    let coll: Result<Collection, &str> = multi.clone().try_into();
+
+    let collection = match coll {
+        // if we can convert it, we have a single Collection; use that!
+        Ok(coll) => {
+            Ok(CollectionSet::from(coll.select(selection).unwrap().try_into()?))
+        },
+        // alt, our only chance is to load everything into memory.
+        Err(_) => {
+            if use_internal_storage {
+                // @CTB warn: loading all the things
+                let coll = multi.load_all_sigs(selection).unwrap();
+                // @CTB multiple selects...
+                Ok(CollectionSet::from(coll.select(selection).unwrap().try_into()?))
+            } else {
+                Err(anyhow::anyhow!("failed. Exiting.").into())
+            }
         }
-        Ok(())
-    } else {
-        Err(anyhow::anyhow!("Signatures failed to load. Exiting.").into())
+    };
+
+    match collection {
+        Ok(collection) => {
+            eprintln!("Indexing {} sketches.", collection.len());
+            let mut index = RevIndex::create(output.as_ref(), collection, colors)?;
+
+            if use_internal_storage {
+                index.internalize_storage()?;
+            }
+            Ok(())
+        },
+        Err(e) => Err(e),
     }
 }
