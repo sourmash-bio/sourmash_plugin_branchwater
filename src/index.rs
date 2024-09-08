a4cc9fff3fe4c82f38e773158e76e97efe1db59c
C. Titus Brown <titus@idyll.org>
2024-09-08 18:08:33 -0400

    fmt

--- src/index.rs
+++ src/index.rs
@@ -3,10 +3,9 @@ use sourmash::index::revindex::RevIndexOps;
 use sourmash::prelude::*;
 use std::path::Path;
 
-use sourmash::Error;
-use sourmash::collection::{Collection, CollectionSet};
-use crate::utils::{ load_collection, ReportType };
 use crate::utils::multicollection::MultiCollection;
+use crate::utils::{load_collection, ReportType};
+use sourmash::collection::{Collection, CollectionSet};
 
 pub fn index<P: AsRef<Path>>(
     siglist: String,
@@ -18,25 +17,31 @@ pub fn index<P: AsRef<Path>>(
 ) -> Result<(), Box<dyn std::error::Error>> {
     println!("Loading siglist");
 
-    let multi: MultiCollection = load_collection(&siglist, selection,
-                                                 ReportType::General,
-                                                 allow_failed_sigpaths).unwrap();
+    let multi: MultiCollection = load_collection(
+        &siglist,
+        selection,
+        ReportType::General,
+        allow_failed_sigpaths,
+    )
+    .unwrap();
     eprintln!("loaded - {}", multi.len());
 
     let coll: Result<Collection, &str> = multi.clone().try_into();
 
     let collection = match coll {
         // if we can convert it, we have a single Collection; use that!
-        Ok(coll) => {
-            Ok(CollectionSet::from(coll.select(selection).unwrap().try_into()?))
-        },
+        Ok(coll) => Ok(CollectionSet::from(
+            coll.select(selection).unwrap().try_into()?,
+        )),
         // alt, our only chance is to load everything into memory.
         Err(_) => {
             if use_internal_storage {
                 // @CTB warn: loading all the things
                 let coll = multi.load_all_sigs(selection).unwrap();
                 // @CTB multiple selects...
-                Ok(CollectionSet::from(coll.select(selection).unwrap().try_into()?))
+                Ok(CollectionSet::from(
+                    coll.select(selection).unwrap().try_into()?,
+                ))
             } else {
                 Err(anyhow::anyhow!("failed. Exiting.").into())
             }
@@ -52,7 +57,7 @@ pub fn index<P: AsRef<Path>>(
                 index.internalize_storage()?;
             }
             Ok(())
-        },
+        }
         Err(e) => Err(e),
     }
 }
