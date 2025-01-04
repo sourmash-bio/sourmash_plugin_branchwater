def630029ec9a5b04cca05f59add32cb3cc3d837
C. Titus Brown <titus@idyll.org>
2025-01-04 08:22:23 -0400

    rm comment

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -83,7 +83,6 @@ pub fn prefetch(
             let overlap = searchsig.count_common(query_mh, false);
             if let Ok(overlap) = overlap {
                 if overlap > 0 && overlap >= threshold_hashes {
-                    // @CTB backport
                     let result = PrefetchResult { overlap, ..result };
                     mm = Some(result);
                 }
