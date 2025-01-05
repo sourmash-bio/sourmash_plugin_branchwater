a285671c2c4cabf4ba256367cd167b67a4ce196c
C. Titus Brown <titus@idyll.org>
2025-01-05 08:18:52 -0400

    refactor out to obj fn

--- src/index.rs
+++ src/index.rs
@@ -1,19 +1,22 @@
+use anyhow::Result;
+
 use sourmash::index::revindex::RevIndex;
 use sourmash::index::revindex::RevIndexOps;
 use sourmash::prelude::*;
 use std::path::Path;
 
 use crate::utils::{load_collection, ReportType};
+use crate::utils::MultiCollection;
 use sourmash::collection::{Collection, CollectionSet};
 
 pub fn index<P: AsRef<Path>>(
     siglist: String,
     selection: Selection,
     output: P,
-    colors: bool,
+    use_colors: bool,
     allow_failed_sigpaths: bool,
     use_internal_storage: bool,
-) -> Result<(), Box<dyn std::error::Error>> {
+) -> Result<()> {
     eprintln!("Loading sketches from {}", siglist);
 
     let multi = match load_collection(
@@ -26,6 +29,16 @@ pub fn index<P: AsRef<Path>>(
         Err(err) => return Err(err.into()),
     };
     eprintln!("Found {} sketches total.", multi.len());
+
+    index_obj(&multi, output, use_colors, use_internal_storage)
+}
+
+pub(crate) fn index_obj<P: AsRef<Path>>(multi: &MultiCollection,
+                        output: P,
+                        use_colors: bool,
+                        use_internal_storage: bool,
+) -> Result<()> {
+    let multi = multi.clone();
 
     // Try to convert it into a Collection and then CollectionSet.
     let collection = match Collection::try_from(multi.clone()) {
@@ -54,7 +67,7 @@ pub fn index<P: AsRef<Path>>(
     match collection {
         Ok(collection) => {
             eprintln!("Indexing {} sketches.", collection.len());
-            let mut index = RevIndex::create(output.as_ref(), collection, colors)?;
+            let mut index = RevIndex::create(output.as_ref(), collection, use_colors)?;
 
             if use_internal_storage {
                 index.internalize_storage()?;
